14dea60543d81c58f5f9852f418e3b5575e634c6
Kiran <edi@blockchainfirm.io>
2022-06-01 10:12:54 +0530

    Transparent layer added

--- emi-calculator/AbstractionLayer.swift
+++ emi-calculator/AbstractionLayer.swift
@@ -13,20 +13,37 @@ class AbstractionLayer : UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addSubview(stack)
-        self.addSubview(bottomView)
-        self.bringSubviewToFront(bottomView)
+        addSubViews()
         self.clipsToBounds = true
         self.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         self.layer.cornerRadius = 20
+       
+        addConstraintsToViews(selectedView: 1)
+        //        generalConstraints()
+    }
+    
+    func addSubViews() {
+        self.addSubview(stack)
+        self.addSubview(bottomView)
+        self.addSubview(topLayer)
+        self.addSubview(middleLayer)
+        self.bringSubviewToFront(topLayer)
+        self.bringSubviewToFront(middleLayer)
+        self.bringSubviewToFront(bottomView)
+    }
+    
+    
+    func addGestures() {
         let tap1 = UITapGestureRecognizer(target: self, action: #selector(tapAction1(_:)))
         view1.addGestureRecognizer(tap1)
         let tap2 = UITapGestureRecognizer(target: self, action: #selector(tapAction2(_:)))
         view2.addGestureRecognizer(tap2)
         let tap3 = UITapGestureRecognizer(target: self, action: #selector(tapAction3(_:)))
         view3.addGestureRecognizer(tap3)
-        addConstraintsToViews(selectedView: 1)
-//        generalConstraints()
+        let tapLayer1 = UITapGestureRecognizer(target: self, action: #selector(tapLayerAction1(_:)))
+        topLayer.addGestureRecognizer(tapLayer1)
+        let tapLayer2 =  UITapGestureRecognizer(target: self, action: #selector(tapLayerAction2(_:)))
+        middleLayer.addGestureRecognizer(tapLayer2)
     }
     
     
@@ -37,26 +54,31 @@ class AbstractionLayer : UIView {
     
     
     
-  
-   
-
+    
+    
+    
     lazy var view1 : UIView = {
         let view = UIView()
-        view.backgroundColor = .blue.withAlphaComponent(0.5)
+        view.backgroundColor = UIColor(hexString: "#243A73")
         view.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         view.layer.cornerRadius = 20
+        let label = UILabel()
+        label.text = "hi Kiran"
+        label.textColor = .white
+        label.textAlignment = .left
+        
         return view
     }()
     
     lazy var view2 : UIView = {
         let view2 = UIView()
-        view2.backgroundColor = .red.withAlphaComponent(0.5)
+        view2.backgroundColor = .red
         return view2
     }()
     
     lazy var view3 : UIView = {
         let view3 = UIView()
-        view3.backgroundColor = .green.withAlphaComponent(0.5)
+        view3.backgroundColor = .green
         return view3
     }()
     
@@ -76,7 +98,22 @@ class AbstractionLayer : UIView {
         view.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         view.layer.cornerRadius = 20
         view.backgroundColor = .orange.withAlphaComponent(0.5)
-       
+        
+        return view
+    }()
+    
+    
+    lazy var topLayer : UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: self.frame.width, height: 200)
+        view.backgroundColor = .black.withAlphaComponent(0.25)
+        return view
+    }()
+    
+    lazy var middleLayer : UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: self.frame.minX, y: self.frame.minY, width: self.frame.width, height: 200)
+        view.backgroundColor = .black.withAlphaComponent(0.25)
         return view
     }()
     
@@ -93,39 +130,63 @@ class AbstractionLayer : UIView {
         addConstraintsToViews(selectedView: 3)
     }
     
+    @objc func tapLayerAction1(_ sender: UITapGestureRecognizer? = nil) {
+        addConstraintsToViews(selectedView: 1)
+    }
+    
+    @objc func tapLayerAction2(_ sender: UITapGestureRecognizer? = nil) {
+        addConstraintsToViews(selectedView: 2)
+    }
+    
     func addConstraintsToViews(selectedView : Int) {
         let heightConstraint1 = NSLayoutConstraint(item: view1, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 150)
         let heightConstraint2 = NSLayoutConstraint(item: view2, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 150)
         let heightConstraint3 = NSLayoutConstraint(item: view3, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 150)
         
+        let topLayerTop = NSLayoutConstraint(item: topLayer, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0)
+        let topLayerBottom = NSLayoutConstraint(item: topLayer, attribute: .bottom, relatedBy: .equal, toItem: view1, attribute: .bottom, multiplier: 1, constant: 0)
+        let middleLayerTop = NSLayoutConstraint(item: middleLayer, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0)
+        let middleLayerBottom = NSLayoutConstraint(item: middleLayer, attribute: .bottom, relatedBy: .equal, toItem: view2, attribute: .bottom, multiplier: 1, constant: 0)
+        
         switch(selectedView) {
         case 1:
             UIView.animate(withDuration: 0.4, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+                self.topLayer.isHidden = true
+                self.middleLayer.isHidden = true
+                
                 self.removeConstraints(self.constraints)
                 self.addConstraints([heightConstraint2,heightConstraint3])
                 print("1 tapped")
                 self.layoutIfNeeded()
             }, completion: nil)
-           
+            
             break
         case 2:
             UIView.animate(withDuration: 0.4, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+                self.topLayer.isHidden = false
+                self.middleLayer.isHidden = true
+                
                 self.removeConstraints(self.constraints)
-                self.addConstraints([heightConstraint1,heightConstraint3])
+                self.addConstraints([heightConstraint1,heightConstraint3, topLayerTop, topLayerBottom])
                 print("2 tapped")
                 self.layoutIfNeeded()
             }, completion: nil)
             break
         case 3:
             UIView.animate(withDuration: 0.4, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+                self.topLayer.isHidden = false
+                self.middleLayer.isHidden = false
+                
                 self.removeConstraints(self.constraints)
-                self.addConstraints([heightConstraint1,heightConstraint2])
+                self.addConstraints([heightConstraint1,heightConstraint2, middleLayerTop, middleLayerBottom])
                 print("3 tapped")
                 self.layoutIfNeeded()
             }, completion: nil)
             break
         default:
             UIView.animate(withDuration: 0.4, delay: 0, options: UIView.AnimationOptions.curveEaseInOut, animations: {
+                self.topLayer.isHidden = true
+                self.middleLayer.isHidden = true
                 self.removeConstraint(heightConstraint1)
                 self.addConstraints([heightConstraint2,heightConstraint3])
                 print("default")
@@ -138,111 +199,37 @@ class AbstractionLayer : UIView {
         let heightConstraint = NSLayoutConstraint(item: self.bottomView, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 100)
         let leading = NSLayoutConstraint(item: self.bottomView, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.leadingAnchor, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 0)
         let trailing = NSLayoutConstraint(item: self.bottomView, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.trailingAnchor, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: 0)
-      
+        
         self.bottomView.addConstraints([heightConstraint,leading,trailing])
         self.layoutIfNeeded()
     }
 }
 
-
-class TaskView: UIView {
-    
-    
-    lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 10.0
-        stackView.alignment = .fill
-        stackView.distribution = .fill
-        stackView.addArrangedSubview(self.firstView)
-        stackView.addArrangedSubview(self.imageView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    //    lazy var btnRemove: UIButton = {
-    //        let btn = UIButton()
-    //        btn.setTitle("Remove", for: .normal)
-    //        btn.setTitleColor(.red, for: .normal)
-    //        btn.layer.borderWidth = 0.5
-    //        btn.layer.borderColor = UIColor.red.cgColor
-    //        let constraint = btn.heightAnchor.constraint(equalToConstant: 30)
-    //        constraint.isActive = true
-    //        constraint.priority = UILayoutPriority(rawValue: 999)
-    //
-    //        btn.addTarget(self, action: #selector(btnRemoveTouchUpInside), for: .touchUpInside)
-    //        return btn
-    //    }()
-    
-    lazy var imageView: UIImageView = {
-        let view = UIImageView()
-        view.layer.masksToBounds = true
-        view.layer.cornerRadius = 10.0
-        let constraint = view.heightAnchor.constraint(equalToConstant: self.frame.height * 0.25)
-        constraint.isActive = true
-        constraint.priority = UILayoutPriority(rawValue: 999)
-        view.contentMode = .scaleToFill
-        view.image = UIImage(named: "man.png")
-        return view
-    }()
-
-    
-    
-    lazy var firstView : UIView = {
-        let view = UIView()
-        let constraint = view.heightAnchor.constraint(equalToConstant: self.frame.height * 0.5)
-        constraint.isActive = true
-        constraint.priority = UILayoutPriority(rawValue: 999)
-        view.backgroundColor = .red
-        return view
-        
-    }()
-    
-    lazy var lblTitle: UILabel = {
-        let lbl = UILabel()
-        lbl.text = "Man.png"
-        return lbl
-    }()
-    
-    lazy var btnShow: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("Hide", for: .normal)
-        btn.setTitleColor(.blue, for: .normal)
-        btn.addTarget(self, action: #selector(btnShowTouchUpInside), for: .touchUpInside)
-        return btn
-    }()
-    
-    //    let delegate: TaskViewDelegate
-    
-    init() {
-        
-        
-        super.init(frame: .zero)
-        backgroundColor = .lightGray
-        addSubview(stackView)
-        //        stackView.snp.makeConstraints { (make) in
-        //            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 5, left: 10, bottom: 10, right: 10))
-        //        }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc
-    func btnShowTouchUpInside() {
-        UIView.animate(withDuration: 0.2) {
-            self.imageView.isHidden = !self.imageView.isHidden
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
         }
-        
-        if self.imageView.isHidden {
-            btnShow.setTitle("Show", for: .normal)
-        } else {
-            btnShow.setTitle("Hide", for: .normal)
-        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
-    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
 }
-
-
-
